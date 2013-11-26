a06662c99e62f600f88a741265a7b13a95e2dfc0
Sven <someone@online.de>
2013-11-26 17:51:15 +0100

    tri-o-sim and joystick class added

---
+++ app/tri-o-sim/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <signal.h>
+
+#include <boost/thread.hpp>
+#include <boost/program_options.hpp>
+#include <boost/timer/timer.hpp> //http://www.boost.org/doc/libs/1_52_0/libs/timer/doc/cpu_timers.html
+
+
+#include "c_joystick.hpp"
+
+using namespace std;
+namespace po = boost::program_options;
+namespace pt = boost::posix_time;
+
+bool execute = true;
+
+cJoystick* Joy;
+
+//***************************************
+void trap(int signal)
+{
+    cout << "\n SIGINT received!" << endl;
+    execute = false;
+}
+
+
+
+int main(int argc, char **argv )
+{
+    boost::timer::auto_cpu_timer t(1,"Program has within the %w s run time the CPU used for %t s (%p \%).\n\n");
+
+    // register Ctrl-C signal
+    signal(SIGINT, &trap);
+
+    std::cout << "Hello world!" << std::endl;
+
+    Joy = new cJoystick;
+
+    cout << " Name of the Joystick: "   << Joy->getName()
+         << ", Buttons: "               << Joy->getNumberOfButtons()
+         << ", Axis: "                  << Joy->getNumberOfAxes()
+         << endl;
+
+
+    while(execute)
+    {
+
+        for(int ii=0; ii < Joy->getNumberOfAxes(); ii++)
+        {
+            cout << " A(" << ii << "): " << Joy->getAxis(ii);
+
+        }
+        cout << endl;
+
+        boost::this_thread::sleep(pt::milliseconds(100)); //arround 10 Hz
+    }
+
+    return(0);
+}
+
---
+++ src/linux/sdl_joystick/c_joystick.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <vector>
+#include <boost/timer/timer.hpp> //http://www.boost.org/doc/libs/1_52_0/libs/timer/doc/cpu_timers.html
+
+
+#include "c_joystick.hpp"
+
+using namespace std;
+namespace pt = boost::posix_time;
+
+cJoystick::cJoystick(int num_joy)
+{
+
+    joy_index   = num_joy;   /* Joystick number. */
+    num_joys    = -1;        /* number of joysticks. */
+
+    int num_axes    = -1;        /* Number of axes. */
+    int num_buttons = -1;        /* Number of buttons. */
+    int num_hats    = -1;        /* Number of hats */
+
+    /* SDL initialization */
+    if (SDL_Init(SDL_INIT_JOYSTICK) < 0)
+    {
+        cerr << "Could not initialize SDL: " <<  SDL_GetError() << endl;
+        return;
+    }
+    num_joys = SDL_NumJoysticks();
+    if (num_joys <= 0)
+    {
+        cerr << "Sorry, no joysticks found." << endl;
+        return;
+    }
+
+    /* Get parameter */
+    if (joy_index >= num_joys)
+    {
+        cerr << "Only " << num_joys
+             << " joystick(s) were found. Indexing starts at zero. Joystick "
+             << joy_index << " does not exist."
+             << endl;
+        return;
+    }
+
+    SDL_JoystickEventState(SDL_ENABLE);
+    m_pJoystick = SDL_JoystickOpen (joy_index);
+
+    /* Collect joystick data. */
+    num_axes = SDL_JoystickNumAxes(m_pJoystick);
+    num_buttons = SDL_JoystickNumButtons(m_pJoystick);
+    num_hats = SDL_JoystickNumHats(m_pJoystick);
+
+    vf_axis.resize(num_axes);
+    vf_button.resize(num_buttons);
+    vf_hat.resize(num_hats);
+
+    m_thread_running = true;
+    m_joy_thread = boost::thread( boost::bind(&cJoystick::loop, this));
+
+}
+
+void cJoystick::loop(void)
+{
+
+    unsigned int ii = 0;
+
+    while(m_thread_running)
+    {
+//        cout << "m_thread_running" << endl;
+
+        while(SDL_PollEvent(&event))
+        {
+            cout << " ----  event -----" << endl;
+
+            switch(event.type)
+            {
+            case SDL_JOYAXISMOTION:  /* Handle Joystick Motion */
+                //if ( ( event.jaxis.value < -3200 ) || (event.jaxis.value > 3200 ) )
+                {
+                    cout << " SDL_JOYAXISMOTION " << endl;
+
+
+                    for (ii = 0; ii < vf_axis.size(); ii++)
+                    {
+                        if( ii == event.jaxis.axis)
+                        {
+                            vf_axis[ii] = SDL_JoystickGetAxis(m_pJoystick, ii) / 32768.0;
+                            cout << " A(" << ii << "): " << vf_axis[ii] << endl;
+
+                            signal_axis(getJoystick(), event.jaxis.axis);
+                        }// if
+                    }// for
+                }// axis event
+                break;
+
+            case SDL_JOYBUTTONDOWN:  /* Handle Joystick Button Presses */
+                for (ii = 0; ii < vf_button.size(); ii++)
+                {
+                    if( ii == event.jbutton.button)
+                    {
+                        vf_button[ii] = SDL_JoystickGetButton(m_pJoystick, ii);
+                        signal_button(getJoystick(), event.jbutton.button);
+                    }// if
+                }// for
+                break;
+            case SDL_QUIT:
+                /* Set whatever flags are necessary to */
+                /* end the main game loop here */
+                m_thread_running = false;
+                break;
+            } // switch
+        }//SDL_event
+
+//        /* Grab current joystick information. */
+//        SDL_JoystickUpdate();
+//
+//        unsigned int ii = 0;
+//        for (ii = 0; ii < vf_axis.size(); ii++)
+//        {
+//            vf_axis[ii] = SDL_JoystickGetAxis(m_pJoystick, ii) / 32768.0;
+//        }
+//
+//        for (ii = 0; ii < vf_button.size(); ii++)
+//        {
+//            vf_button[ii] = SDL_JoystickGetButton(m_pJoystick, ii);
+//        }
+//
+//        for (ii = 0; ii < vf_hat.size(); ii++)
+//        {
+//            vf_hat[ii] = SDL_JoystickGetHat(m_pJoystick, ii);
+//        }
+
+        boost::this_thread::sleep(pt::milliseconds(10)); //around 100 Hz
+    }// thread_running
+
+}
+
+
+int cJoystick::getNumberOfButtons ()
+{
+    return(vf_button.size());
+}
+
+int cJoystick::getNumberOfAxes ()
+{
+    return(vf_axis.size());
+}
+
+const char *cJoystick::getName()
+{
+    return(SDL_JoystickName(joy_index));
+}
+
+const cJoystick* cJoystick::getJoystick()
+{
+    return( (const cJoystick*)m_pJoystick );
+}
+
+float cJoystick::getAxis(int axis)
+{
+    if(axis > 0 && axis < (int)vf_axis.size())
+    {
+        return( vf_axis[axis] );
+    }
+    else
+        return(0.);
+}
+
+int cJoystick::getButton(int button)
+{
+    if(button > 0 && button < (int)vf_button.size())
+    {
+        return( vf_button[button] );
+    }
+    else
+        return(-1);
+}
+
+
+cJoystick::~cJoystick()
+{
+    m_thread_running = false;
+    m_joy_thread.join();
+
+    SDL_JoystickClose(m_pJoystick);
+    SDL_Quit();
+}
