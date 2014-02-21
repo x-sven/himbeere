caf23f634748e7b3c3357425e972ee5fdcdeacf8
Sven <someone@online.de>
2014-02-21 21:27:34 +0100

    ServoInterface class for reading added, project controller_test added

---
+++ app/control_tester/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <signal.h>
+
+
+#include "Millis/millis.h"
+
+#include "HardwareSerial/HardwareSerial.h"
+#include "ServoInterface/ServoInterface.h"
+
+using namespace std;
+
+//HardwareSerial Serial("/dev/ttyUSB0");
+HardwareSerial Serial("/dev/ttyAMA0");
+cServoInterfaceClass fcci;
+
+bool execute = true;
+
+//***************************************
+void trap(int signal)
+{
+    cout << "\n SIGINT received!" << endl;
+    execute = false;
+}
+
+
+//---------------------------------------------------------
+void setup()
+{
+    Serial.begin(38400);
+    fcci.begin(&Serial);
+}
+
+//---------------------------------------------------------
+int main()
+{
+    setup();
+
+    // register Ctrl-C signal
+    signal(SIGINT, &trap);
+
+    while(execute)
+    {
+
+        fcci.update();
+
+        for(uint8_t ch = 0; ch < fcci.num_channel_max; ch++)
+        {
+            printf("%d ", fcci.get_channel(ch));
+        }
+        printf("\n");
+
+
+        //printf("0x%d\n", data);
+        boost::this_thread::sleep(boost::posix_time::milliseconds(10));
+    }
+    Serial.end();
+    return(0);
+}
---
+++ src/linux/ServoInterface/ServoInterface.cpp
@@ -0,0 +1,131 @@
+#include "ServoInterface.h"
+
+cServoInterfaceClass::cServoInterfaceClass(Stream *stream):
+    num_channel_max(6),
+    m_stream(NULL),
+    reciever_state(0),
+    reciever_couter(0),
+    rx_channel(NULL)
+{
+    rx_channel = (uint16_t*)malloc(num_channel_max*sizeof(uint16_t));
+
+    memset(rx_channel, 0, 6);
+    if(NULL != stream)
+    {
+        begin(stream);
+    }
+}
+
+void cServoInterfaceClass::begin(Stream *stream)
+{
+    if(NULL != stream)
+    {
+        m_stream = stream;
+    }
+}
+
+void cServoInterfaceClass::update(void)
+{
+    uint8_t bytedata = 0;
+    int16_t numc = 0;
+
+    numc = m_stream->available();
+    if(0 < numc )
+    {
+        // printf("Number bytes: %d \n",numc); fflush(stdout);
+        for (int16_t i=0; i < numc; i++)
+        {
+
+            bytedata = m_stream->read();
+            //printf("0x%x ",bytedata);
+            switch(reciever_state)
+            {
+
+            case 0:
+                if(bytedata==(uint8_t)0xff)
+                    reciever_state=1;
+                else
+                {
+                    reciever_state=0;
+                    reciever_couter=0;
+                }
+                break;
+
+            case 1:
+                if(bytedata==0xfe)
+                    reciever_state=2;
+                else
+                {
+                    reciever_state=0;
+                    reciever_couter=0;
+                }
+                break;
+
+            case 2:
+
+                if(reciever_couter%2==0)
+                {
+                    rx_channel[reciever_couter/2]=bytedata<<8;
+                }
+                else
+                {
+                    rx_channel[(unsigned int)reciever_couter/2] |= bytedata;
+                }
+
+                reciever_couter++;
+
+                if(bytedata==0xfe )
+                {
+                    reciever_couter=0;
+                    reciever_state=0;
+                }
+                if(bytedata==0xff)
+                {
+                    reciever_couter=0;
+                    reciever_state=1;
+                }
+
+
+                if(reciever_couter>11) // 6x2 bytes in rx_channel
+                {
+                    reciever_couter=0;
+                    reciever_state=3;
+                }
+                break;
+            case 3:
+//                    printf("\n");
+//                printf("%d %d %d %d %d %d\n",rx_channel[0],rx_channel[1],rx_channel[2],rx_channel[3],rx_channel[4],rx_channel[5]);
+
+// TODO (sven#1#): CRC check here?
+
+                reciever_state=0;
+                break;
+            default:
+                ;
+
+            }//switch case
+        }//for(numc)
+    }//if()
+}
+
+uint16_t cServoInterfaceClass::get_channel(uint8_t channel)
+{
+    if(0 < channel) // && channel < num_ch_max
+    {
+        return(rx_channel[channel]);
+    }
+    else
+    {
+        return(0);
+    }
+}
+
+
+cServoInterfaceClass::~cServoInterfaceClass()
+{
+    if(NULL != rx_channel)
+    {
+        free(rx_channel);
+    }
+}
+
